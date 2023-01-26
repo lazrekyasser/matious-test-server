9346f9c335d007e270364f894cc6d75bcc40100a
lazrekyasser <lazrekyasser1998@gmail.com>
2023-01-27 00:43:53 +0100

    add the vercel config

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ mongoose.connection.on('error', (error) => console.error(error));
 mongoose.connection.once('open', () => console.log('Connected to Database!'))
 //
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.send('welcom to the super market sales api ');
+})
 //routes
-const productLineRouter = require('./routes/productLines');
-const customerTypeGenderRouter = require('./routes/customerTypeGender');
-const moyenneRatingPerGenderRouter = require('./routes/ratingPerGender');
-app.use('/product-line', productLineRouter);
-app.use('/customer-type-gender', customerTypeGenderRouter);
-app.use('/moy-rating-per-gender', moyenneRatingPerGenderRouter);
+mongoose.connection.on('connected', () => {
+    const productLineRouter = require('./routes/productLines');
+    const customerTypeGenderRouter = require('./routes/customerTypeGender');
+    const moyenneRatingPerGenderRouter = require('./routes/ratingPerGender');
+    app.use('/product-line', productLineRouter);
+    app.use('/customer-type-gender', customerTypeGenderRouter);
+    app.use('/moy-rating-per-gender', moyenneRatingPerGenderRouter);
+})
 
-app.listen(3000, () => console.log('server starded...'))
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log('server starded...'))
